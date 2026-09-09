7a01d16b035c813f898fc63a9e21fb68b69fbd21
Bryan Thomas <bryanrt.bt@gmail.com>
2026-09-08 21:37:19 -0600

    final changes before presentation. Cleaned wording

---
+++ R/pull_scorecard.R
@@ -0,0 +1,47 @@
+source(here::here("R/db.R"))
+
+get_cc_scorecard_data <- function(performance_window, write = TRUE) {
+  conn <- db_connect()
+  on.exit(DBI::dbDisconnect(conn))
+
+  # -- Type contract (Postgres DDL source) ---------------------------------
+  # sq_num         INTEGER      NOT NULL
+  # user_ref_num   VARCHAR(14)  NOT NULL   join key -> apps
+  # score          NUMERIC      NULL
+  # segment        TEXT         NULL       Rmd: as.character(as.numeric(segment))
+  # actduty        TEXT         NULL
+  # trans_date_ct  DATE         NOT NULL   date filter column
+  # proc_date_ct   DATE         NULL
+  # primemdt       TEXT         NULL
+
+  df <- DBI::dbGetQuery(conn, glue::glue("
+    SELECT
+      sq_num,
+      user_ref_num,
+      score,
+      segment,
+      actduty,
+      trans_date_ct,
+      proc_date_ct,
+      primemdt
+    FROM scorecard
+    WHERE trans_date_ct >= '{lubridate::floor_date(performance_window[1], 'month')}'
+      AND trans_date_ct <= '{lubridate::ceiling_date(performance_window[1], 'month') - 1}'
+  "))
+
+  if (write == TRUE) {
+    data.table::fwrite(
+      x = df,
+      file = glue::glue(here::here(
+        "data/scorecard/scorecard_{format(as.Date(min(performance_window)), '%Y%m')}.txt.gz"
+      )),
+      sep = ",",
+      compress = "auto",
+      append = FALSE,
+      scipen = 999,
+      showProgress = TRUE
+    )
+  }
+
+  return(df)
+}
